804aac2a9664fe08bbc9801f47b59fcd401c6391
Maciej Soltys <maciej.soltys@student.wat.edu.pl>
2023-08-23 22:01:33 +0200

    Restructurized client-app folder

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import './App.css';
 import axios from 'axios';
 import { Header, List } from 'semantic-ui-react';
 
@@ -9,7 +8,6 @@ function App() {
   useEffect(() => {
     axios.get('http://localhost:5000/api/recipes')
       .then(response => {
-        console.log(response);
         setRecipes(response.data);
       })
   }, [])
