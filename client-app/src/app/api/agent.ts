85e9b09c9cf8d23db23e2a35fd22b807601871e5
Maciej Soltys <maciej.soltys@student.wat.edu.pl>
2023-09-02 18:14:19 +0200

    Added bad Guid handling

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -16,10 +16,13 @@ axios.interceptors.response.use(async response => {
     await sleep(1000);
     return response;
 }, (error: AxiosError) => {
-    const {data, status} = error.response as AxiosResponse;
+    const {data, status, config} = error.response as AxiosResponse;
 
     switch (status) {
         case 400:
+            if (config.method === 'get' && data.errors.hasOwnProperty('id')) {
+                router.navigate('/not-found');
+            }
             if (data.errors) {
                 const modalStateErrors = [];
                 for (const key in data.errors) {
