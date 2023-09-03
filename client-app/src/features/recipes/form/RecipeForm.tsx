47c15f53f46b341a80b51dfa2cdd5ac552b3b24a
Maciej Soltys <maciej.soltys@student.wat.edu.pl>
2023-09-03 12:11:59 +0200

    Changed RecipeForm to use Formik

--- client-app/src/features/recipes/form/RecipeForm.tsx
+++ client-app/src/features/recipes/form/RecipeForm.tsx
@@ -1,12 +1,12 @@
 import React, { ChangeEvent, useEffect, useState } from 'react';
-import { Button, Form, Segment } from 'semantic-ui-react';
+import { Button, Segment } from 'semantic-ui-react';
 import { useStore } from '../../../app/stores/store';
 import { observer } from 'mobx-react-lite';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import { Recipe } from '../../../app/models/recipe';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
 import { v4 as uuid } from 'uuid';
-import { Formik } from 'formik';
+import { Formik, Form, Field } from 'formik';
 
 export default observer(function RecipeForm() {
     const { recipeStore } = useStore();
@@ -45,32 +45,24 @@ export default observer(function RecipeForm() {
     return (
         <Segment clearing>
             <Formik enableReinitialize initialValues={recipe} onSubmit={(values) => console.log(values)}>
-                {({ values: recipe, handleChange, handleSubmit }) => (
-                    <Form onSubmit={handleSubmit} autoComplete="off">
-                        <Form.Input
+                {({ handleSubmit }) => (
+                    <Form className='ui form' onSubmit={handleSubmit} autoComplete="off">
+                        <Field
                             placeholder="Title"
-                            value={recipe.title}
                             name="title"
-                            onChange={handleChange}
                         />
-                        <Form.TextArea
+                        <Field
                             placeholder="Description"
-                            value={recipe.description}
                             name="description"
-                            onChange={handleChange}
                         />
-                        <Form.Input
+                        <Field
                             placeholder="Category"
-                            value={recipe.category}
                             name="category"
-                            onChange={handleChange}
                         />
-                        <Form.Input
+                        <Field
                             type="date"
                             placeholder="Date"
-                            value={recipe.date}
                             name="date"
-                            onChange={handleChange}
                         />
                         <Button loading={loading} floated="right" positive type="submit" content="Submit" />
                         <Button as={Link} to="/recipes" floated="right" type="button" content="Cancel" />
