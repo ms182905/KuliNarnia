f560a22fd16fbad50a5d97944926bb40b4615369
Maciej Soltys <maciej.soltys@student.wat.edu.pl>
2023-09-03 12:40:06 +0200

    Added Yup and title validation

--- client-app/src/features/recipes/form/RecipeForm.tsx
+++ client-app/src/features/recipes/form/RecipeForm.tsx
@@ -1,12 +1,13 @@
 import React, { ChangeEvent, useEffect, useState } from 'react';
-import { Button, Segment } from 'semantic-ui-react';
+import { Button, FormField, Label, Segment } from 'semantic-ui-react';
 import { useStore } from '../../../app/stores/store';
 import { observer } from 'mobx-react-lite';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import { Recipe } from '../../../app/models/recipe';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
 import { v4 as uuid } from 'uuid';
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
 export default observer(function RecipeForm() {
     const { recipeStore } = useStore();
@@ -20,6 +21,10 @@ export default observer(function RecipeForm() {
         category: '',
         description: '',
         date: '',
+    });
+
+    const validationSchema = Yup.object({
+        title: Yup.string().required('The recipe title is required'),
     });
 
     useEffect(() => {
@@ -44,26 +49,16 @@ export default observer(function RecipeForm() {
 
     return (
         <Segment clearing>
-            <Formik enableReinitialize initialValues={recipe} onSubmit={(values) => console.log(values)}>
+            <Formik validationSchema={validationSchema} enableReinitialize initialValues={recipe} onSubmit={(values) => console.log(values)}>
                 {({ handleSubmit }) => (
-                    <Form className='ui form' onSubmit={handleSubmit} autoComplete="off">
-                        <Field
-                            placeholder="Title"
-                            name="title"
-                        />
-                        <Field
-                            placeholder="Description"
-                            name="description"
-                        />
-                        <Field
-                            placeholder="Category"
-                            name="category"
-                        />
-                        <Field
-                            type="date"
-                            placeholder="Date"
-                            name="date"
-                        />
+                    <Form className="ui form" onSubmit={handleSubmit} autoComplete="off">
+                        <FormField>
+                            <Field placeholder="Title" name="title" />
+                            <ErrorMessage name='title' render={(error: string) => <Label basic color='red' content={error} />}/>
+                        </FormField>
+                        <Field placeholder="Description" name="description" />
+                        <Field placeholder="Category" name="category" />
+                        <Field type="date" placeholder="Date" name="date" />
                         <Button loading={loading} floated="right" positive type="submit" content="Submit" />
                         <Button as={Link} to="/recipes" floated="right" type="button" content="Cancel" />
                     </Form>
