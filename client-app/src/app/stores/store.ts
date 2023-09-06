55e93d1c05c772574ff0a5429854a2b1f0b2267b
Maciej Soltys <maciej.soltys@student.wat.edu.pl>
2023-09-06 21:35:05 +0200

    Added userStore

--- client-app/src/app/stores/store.ts
+++ client-app/src/app/stores/store.ts
@@ -1,15 +1,18 @@
 import { createContext, useContext } from "react";
 import RecipeStore from "./recipeStore";
 import CommonStore from "./commonStore";
+import UserStore from "./userStore";
 
 interface Store {
-    recipeStore: RecipeStore
-    commonStore: CommonStore
+    recipeStore: RecipeStore;
+    commonStore: CommonStore;
+    userStore: UserStore;
 }
 
 export const store: Store = {
     recipeStore: new RecipeStore(),
-    commonStore: new CommonStore()
+    commonStore: new CommonStore(),
+    userStore: new UserStore()
 }
 
 export const StoreContext = createContext(store);
--- client-app/src/features/users/LoginForm.tsx
+++ client-app/src/features/users/LoginForm.tsx
@@ -1,21 +1,24 @@
 import { Form, Formik } from "formik";
 import MyTextInput from "../../app/common/form/MyTextInput";
 import { Button } from "semantic-ui-react";
+import { useStore } from "../../app/stores/store";
+import { observer } from "mobx-react-lite";
 
-export default function LoginForm() {
+export default observer(function LoginForm() {
+    const {userStore} = useStore();
     return (
         <Formik
-            initialValues={{email: '', pasword: ''}}
-            onSubmit={values => console.log(values)}
+            initialValues={{email: '', password: ''}}
+            onSubmit={values => userStore.login(values)}
         >
-            {({handleSubmit}) => (
+            {({handleSubmit, isSubmitting}) => (
                 <Form className='ui form' onSubmit={handleSubmit} autoComplete="off">
                     <MyTextInput placeholder='Email' name='email' />
                     <MyTextInput placeholder='Password' name='password' type='password' />
-                    <Button positive content='Login' type="submit" fluid /> 
+                    <Button loading={isSubmitting} positive content='Login' type="submit" fluid /> 
                 </Form>
             )}
             
         </Formik>
     )
-}
+})
