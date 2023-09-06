dc915595fc8349573d1159a94b100277d2aaa669
Maciej Soltys <maciej.soltys@student.wat.edu.pl>
2023-09-06 23:05:11 +0200

    Added modalStore and ModalContainer

--- client-app/src/app/stores/store.ts
+++ client-app/src/app/stores/store.ts
@@ -2,17 +2,20 @@ import { createContext, useContext } from "react";
 import RecipeStore from "./recipeStore";
 import CommonStore from "./commonStore";
 import UserStore from "./userStore";
+import ModalStore from "./modalStore";
 
 interface Store {
     recipeStore: RecipeStore;
     commonStore: CommonStore;
     userStore: UserStore;
+    modalStore: ModalStore;
 }
 
 export const store: Store = {
     recipeStore: new RecipeStore(),
     commonStore: new CommonStore(),
-    userStore: new UserStore()
+    userStore: new UserStore(),
+    modalStore: new ModalStore()
 }
 
 export const StoreContext = createContext(store);
--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -2,9 +2,10 @@ import { Link } from 'react-router-dom';
 import { Container, Header, Segment, Image, Button } from 'semantic-ui-react';
 import { useStore } from '../../app/stores/store';
 import { observer } from 'mobx-react-lite';
+import LoginForm from '../users/LoginForm';
 
 export default observer(function HomePage() {
-    const { userStore } = useStore();
+    const { userStore, modalStore } = useStore();
 
     return (
         <Segment inverted textAlign="center" vertical className="masthead">
@@ -16,11 +17,18 @@ export default observer(function HomePage() {
                 {userStore.isLoggedIn ? (
                     <></>
                 ) : (
-                    <Header as="h2">
-                        <Button as={Link} to="/login" size="huge" inverted>
-                            Login
-                        </Button>
-                    </Header>
+                    <>
+                        <Header as="h2">
+                            <Button Button onClick={() => modalStore.openModal(<LoginForm />)} size="huge" inverted>
+                                Login
+                            </Button>
+                        </Header>
+                        <Header as="h2">
+                            <Button Button onClick={() => modalStore.openModal(<h1>Register</h1>)} size="huge" inverted>
+                                Register
+                            </Button>
+                        </Header>
+                    </>
                 )}
                 <Header as="h2">
                     <Button as={Link} to="/recipes" size="huge" inverted>
