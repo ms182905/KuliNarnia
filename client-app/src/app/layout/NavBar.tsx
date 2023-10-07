9cc4f4805aabdc8f86d1470f78643cc34181e370
Maciej Soltys <maciej.soltys@student.wat.edu.pl>
2023-10-07 23:25:19 +0200

    Added displaying some features only for logged user.

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -2,11 +2,13 @@ import { Button, Container, Menu, Image, Dropdown } from 'semantic-ui-react';
 import { Link, NavLink } from 'react-router-dom';
 import { useStore } from '../stores/store';
 import { observer } from 'mobx-react-lite';
-import UserStore from '../stores/userStore';
+import LoginForm from '../../features/users/LoginForm';
+import RegisterForm from '../../features/users/RegisterForm';
 
 export default observer(function NavBar() {
     const {
         userStore: { user, logout },
+        modalStore: { openModal }
     } = useStore();
     return (
         <Menu inverted fixed="top">
@@ -17,18 +19,22 @@ export default observer(function NavBar() {
                 </Menu.Item>
                 <Menu.Item as={NavLink} to="/recipes" name="Recipes" />
                 <Menu.Item as={NavLink} to="/errors" name="Errors" />
-                <Menu.Item as={NavLink} to="/favouriteRecipes" name="Favourites" />
-                <Menu.Item>
-                    <Button as={NavLink} to="/createRecipe" positive content="Create Recipe" />
-                </Menu.Item>
+                {user && 
+                    <Menu.Item as={NavLink} to="/favouriteRecipes" name="Favourites" /> &&
+                    <Menu.Item>
+                        <Button as={NavLink} to="/createRecipe" positive content="Create Recipe" />
+                    </Menu.Item>}
                 <Menu.Item position="right">
                     <Image src={user?.image || '/assets/user.png'} avatar spaced="right" />
                     <Dropdown pointing="top left" text={user?.displayName}>
-                        <Dropdown.Menu>
-                            {/*TODO: Add login if not user logged */}
+                        {user ? (<Dropdown.Menu>
                             <Dropdown.Item as={Link} to={`/profile/${user?.username}`} text="My Profile" icon="user" />
+                            <Dropdown.Item as={Link} to={'/userRecipes'} text="My recipes" icon="birthday cake" />
                             <Dropdown.Item onClick={logout} text="Logout" icon="power" />
-                        </Dropdown.Menu>
+                        </Dropdown.Menu>) : (<Dropdown.Menu>
+                            <Dropdown.Item onClick={() => openModal(<LoginForm />)} text="Login" icon="user" />
+                            <Dropdown.Item onClick={() => openModal(<RegisterForm />)} text="Register" icon="user plus" />
+                        </Dropdown.Menu>)}
                     </Dropdown>
                 </Menu.Item>
             </Container>
