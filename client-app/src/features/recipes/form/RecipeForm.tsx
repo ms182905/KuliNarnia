e8f5421aaa627920bb53ad2b57e0fd5d78d4a777
Maciej Soltys <maciej.soltys@student.wat.edu.pl>
2023-10-30 15:28:59 +0100

    Added preview of file selection in RecipeForm.

--- client-app/src/features/recipes/form/RecipeForm.tsx
+++ client-app/src/features/recipes/form/RecipeForm.tsx
@@ -1,8 +1,8 @@
-import { useEffect } from 'react';
-import { Header, Segment } from 'semantic-ui-react';
+import { ChangeEvent, useEffect, useState } from 'react';
+import { Button, Header, Icon, Segment } from 'semantic-ui-react';
 import { useStore } from '../../../app/stores/store';
 import { observer } from 'mobx-react-lite';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
 import RecipeFormInstructions from './RecipeFormInstructions';
 import RecipeFormIngredients from './RecipeFormIngredients';
@@ -15,6 +15,8 @@ export default observer(function RecipeForm() {
     const { loadTags } = tagStore;
     const { loadMeasurements } = measurementStore;
     const { id } = useParams();
+
+    const [selectedFile, setSelectedFile] = useState<File | null>(null);
 
     useEffect(() => {
         if (id && (!recipeStore.selectedRecipe || recipeStore.selectedRecipe.id !== id)) {
@@ -49,6 +51,14 @@ export default observer(function RecipeForm() {
     //         updateRecipe(recipe).then(() => navigate(`/recipes/${recipe.id}`));
     //     }
     // }
+    
+        const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+            const files = event.target.files;
+            if (files && files.length > 0) {
+                setSelectedFile(files[0]);
+            }
+        };
+    
 
     if (recipeStore.loadingInitial) return <LoadingComponent content="Loading recipe..." />;
     if (categoryStore.loadingInitial) return <LoadingComponent content="Loading categories..." />;
@@ -70,7 +80,7 @@ export default observer(function RecipeForm() {
             </Segment>
             <RecipeFormBaseInfo />
 
-            <Segment textAlign="center" inverted color="blue" style={{ border: 'none', borderRadius: '3px' }}>
+            <Segment textAlign="center" inverted color="pink" style={{ border: 'none', borderRadius: '3px' }}>
                 <Header>Ingredients</Header>
             </Segment>
 
@@ -79,7 +89,37 @@ export default observer(function RecipeForm() {
             <Segment textAlign="center" inverted color="violet" style={{ border: 'none', borderRadius: '3px' }}>
                 <Header>Instructions</Header>
             </Segment>
+
             <RecipeFormInstructions />
+
+            <Segment textAlign="center" inverted color="yellow" style={{ border: 'none', borderRadius: '3px' }}>
+                <Header>Photos</Header>
+            </Segment>
+            <Segment placeholder>
+            {selectedFile ? (
+                // Display the selected file
+                <div>
+                    <Icon name="file image outline" size="huge" />
+                    <p>Selected file: {selectedFile.name}</p>
+                </div>
+            ) : (
+                // Placeholder for selecting a file
+                <input
+                    type="file"
+                    id="fileInput"
+                    accept="image/*"
+                    onChange={handleFileChange}
+                    style={{ display: 'none' }}
+                />
+            )}
+            <label htmlFor="fileInput" className="ui primary button">
+                <i className="upload icon"></i> Upload Photo
+            </label>
+        </Segment>
+            <Segment>
+                {/* <Button as={Link} to={`/recipes/${recipeStore.selectedRecipe.id}`} color="teal" floated="right" content="View" /> */}
+                <Button color="green" size="huge" fluid content="Save changes" />
+            </Segment>
         </Segment.Group>
     );
 });
