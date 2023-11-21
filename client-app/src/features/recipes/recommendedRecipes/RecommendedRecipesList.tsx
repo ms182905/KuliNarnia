4f09fa307cc1668eab22d6cfba97ce82faa98832
Maciej Soltys <maciej.soltys@student.wat.edu.pl>
2023-11-21 22:50:43 +0100

    Styled recommended recipes. Deleted unused RecommendedRecipesListItem element.

--- client-app/src/features/recipes/recommendedRecipes/RecommendedRecipesList.tsx
+++ client-app/src/features/recipes/recommendedRecipes/RecommendedRecipesList.tsx
@@ -1,31 +1,30 @@
-import { Header } from 'semantic-ui-react';
 import { useStore } from '../../../app/stores/store';
 import { useState } from 'react';
-import RecommendedRecipesListItem from './RecommendedRecipesListItem';
+import RecipeListItem from '../dashboard/RecipeListItem';
 
 export default function RecommendedRecipesList() {
     const { recommendedRecipesStore } = useStore();
     const { recommendedRecipes, recommendedRecipeRegistry } = recommendedRecipesStore;
 
-    const [registrySize] = useState(recommendedRecipeRegistry.size);
-
-    if (registrySize < 1) {
-        return (
-            <>
-                <Header as="h2" textAlign="center" attached="bottom" style={{ border: '10px' }}>
-                    Cannot determine recommended recipes!
-                </Header>
-                <Header as="h2" textAlign="center" attached="bottom" style={{ border: '10px' }}>
-                    Try to explore more recipes
-                </Header>
-            </>
-        );
-    }
+    useState(recommendedRecipeRegistry.size);
 
     return (
         <>
-            {recommendedRecipes.map((recipe) => (
-                <RecommendedRecipesListItem key={recipe.id} recipe={recipe} />
+            <div
+                id="index"
+                style={{ gridTemplateAreas: "'text'", textAlign: 'center', gridTemplateColumns: '1fr' }}
+                className="card__content"
+            >
+                {recommendedRecipes.length > 0 ? (
+                    <h2 style={{ padding: '0.5em' }}>Recipes recommended to You</h2>
+                ) : (
+                    <h2 style={{ padding: '0.5em' }}>
+                        Cannot determine recommended recipes! <br /> Try to explore some recipes first!
+                    </h2>
+                )}
+            </div>
+            {recommendedRecipes.map((recipe, index) => (
+                <RecipeListItem index={index} recipe={recipe} />
             ))}
         </>
     );
