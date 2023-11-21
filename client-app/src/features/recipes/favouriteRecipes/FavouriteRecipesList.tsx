c48dbf27c1947abf21277da351a06776b8aaac33
Maciej Soltys <maciej.soltys@student.wat.edu.pl>
2023-11-21 22:01:25 +0100

    Deleted unused file.

--- client-app/src/features/recipes/favouriteRecipes/FavouriteRecipesList.tsx
+++ client-app/src/features/recipes/favouriteRecipes/FavouriteRecipesList.tsx
@@ -1,6 +1,4 @@
-import { Header } from 'semantic-ui-react';
 import { useStore } from '../../../app/stores/store';
-import FavouriteRecipesListItem from './FavouriteRecipesListItem';
 import { useState } from 'react';
 import RecipeListItem from '../dashboard/RecipeListItem';
 
@@ -8,15 +6,7 @@ export default function FavouriteRecipesList() {
     const { favouriteRecipesStore } = useStore();
     const { favouriteRecipes, favouriteRecipeRegistry } = favouriteRecipesStore;
 
-    const [registrySize] = useState(favouriteRecipeRegistry.size);
-
-    if (registrySize < 1) {
-        return (
-            <Header as="h3" textAlign="center" attached="bottom" style={{ border: '10px' }}>
-                No favourite recipes yet!
-            </Header>
-        );
-    }
+    useState(favouriteRecipeRegistry.size);
 
     return (
         <>
